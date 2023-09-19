321aee6d86dedf2b08616896b54697f99d9be0fe
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-09-18 21:30:36 -0600

    Remove unused code

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -6,9 +6,22 @@ export default function App({ Component }: AppProps) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Jose-Fabio-CV</title>
+        <title>Jose Fabio CV</title>
       </head>
       <body>
+        <header>
+          <nav>
+            <h1>Jose Fabio</h1>
+            <ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/about">About me</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
         <Component />
       </body>
     </html>
