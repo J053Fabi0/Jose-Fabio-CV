3b5a95b31e2ec7f00ba6a6e503e8af3a4a4d5d04
Jose Fabio <J053Fabi0@users.noreply.github.com>
2025-02-24 16:00:15 -0600

    Set correct zoom for pdf

--- script.js
+++ script.js
@@ -3,4 +3,7 @@ pdfjsLib.GlobalWorkerOptions.workerSrc =
   "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.12.313/pdf.worker.min.js";
 
 var pdfViewer = new PDFjsViewer($("body"));
-pdfViewer.loadDocument("./figures/jose-fabio-arguello-loya-resume.pdf");
+pdfViewer.loadDocument("./figures/jose-fabio-arguello-loya-resume.pdf").then(() => {
+  pdfViewer.setZoom("width");
+  setInterval(() => pdfViewer.refreshAll(), 1000);
+});
