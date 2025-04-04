bb388ae6ccfff1e39744011efa256953902e28b6
Jose Fabio <J053Fabi0@users.noreply.github.com>
2025-04-04 16:33:31 -0600

    Add my first project to the portafolio

--- routes/portafolio.tsx
+++ routes/portafolio.tsx
@@ -1,12 +1,34 @@
 import { Head } from "$fresh/runtime.ts";
 
+interface Project {
+  title: string;
+  from: string;
+  to: string;
+  description: string;
+  images: string[];
+}
+
+const projets: Project[] = [
+  {
+    title: "Mining Nodes Service",
+    from: "07/2023",
+    to: "05/2024",
+    description:
+      "Service for crypto enthusiasts. Automated the creation of mining nodes in the Incognito blockchain.",
+    images: [
+      "./portafolio/mining-nodes-1.png",
+      "./portafolio/mining-nodes-2.png",
+    ],
+  },
+];
+
 export default function Portafolio() {
   return (
     <>
       <Head>
         <title>Portafolio Jose Fabio</title>
       </Head>
-      <div className="container pl-16 pt-16">
+      <div className="container px-6 pt-16 md:pl-16">
         <h1 className="m-0 mb-8 text-3xl font-normal">
           Some of my latest works
           <br />
@@ -14,32 +36,28 @@ export default function Portafolio() {
         </h1>
 
         <div className="flex flex-col gap-4">
-          <header>
-            <h2 className="text-xl font-semibold">Badger</h2>
-            <p>
-              Product design — 2022 —{"  "}
-              <a className="" href="https://getbadger.io/">
-                getbadger.io
-              </a>
-            </p>
-          </header>
-          <p>
-            Badger is a new product from marketgoo to generate and share SEO
-            reports for agencies and professional freelances.
-          </p>
-          <p>It's still in progress. Some early mockups:</p>
+          {projets.map((p) => (
+            <>
+              <header>
+                <h2 className="text-xl font-semibold">{p.title}</h2>
+                <p>
+                  {p.from} — {p.to}
+                </p>
+              </header>
+              <p>{p.description}</p>
 
-          <div className="carousel no-scrollbar flex snap-x gap-4 overflow-x-auto border-x border-gray-900">
-            {new Array(10).fill(0).map(() => (
-              <figure className="ml-[-1px] min-w-[800px] snap-start">
-                <img
-                  src="https://oscarotero.com/portfolio/badger/mockup.webp"
-                  alt="Mockup 1"
-                  className="block w-full border border-gray-900"
-                />
-              </figure>
-            ))}
-          </div>
+              <div className="carousel no-scrollbar flex snap-x gap-4 overflow-x-auto border-x border-gray-900">
+                {p.images.map((url) => (
+                  <figure className="ml-[-1px] min-w-[min(100%,800px)] snap-start">
+                    <img
+                      src={url}
+                      className="block h-full w-full border border-gray-900 object-cover"
+                    />
+                  </figure>
+                ))}
+              </div>
+            </>
+          ))}
         </div>
       </div>
     </>
