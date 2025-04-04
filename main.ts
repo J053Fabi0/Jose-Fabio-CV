9711645bc0067d7c34580213f908be8c29af4928
Jose Fabio <J053Fabi0@users.noreply.github.com>
2025-04-03 22:52:08 -0600

    Fix std import

--- main.ts
+++ main.ts
@@ -4,10 +4,8 @@
 /// <reference no-default-lib="true" />
 /// <reference lib="dom.asynciterable" />
 
-import "$std/dotenv/load.ts";
-
-import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 import config from "./fresh.config.ts";
+import { start } from "$fresh/server.ts";
 
 await start(manifest, config);
